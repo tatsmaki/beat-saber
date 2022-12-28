28c170791ea4b8845cda9365a977e96e015bf1e3
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-12-28 23:10:14 +0100

    fix: 90 deg rotation

--- src/components/ground.ts
+++ src/components/ground.ts
@@ -1,9 +1,10 @@
 import { Mesh, MeshLambertMaterial, PlaneGeometry } from "three";
+import { degToRad } from "three/src/math/MathUtils";
 
-const geometry = new PlaneGeometry(0.5, 0.5);
+const geometry = new PlaneGeometry(10, 10);
 const material = new MeshLambertMaterial({ color: 0xffffff });
 
 export const ground = new Mesh(geometry, material);
 
 ground.position.set(0, 0, 0);
-ground.rotation.set(90, 0, 0);
+ground.rotation.set(0, degToRad(90), 0);
