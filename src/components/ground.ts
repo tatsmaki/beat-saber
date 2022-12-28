f845968bcb01c869137f62da0ee68f2fdcf90cf7
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-12-28 23:12:09 +0100

    fix: ground rotation

--- src/components/ground.ts
+++ src/components/ground.ts
@@ -7,4 +7,4 @@ const material = new MeshLambertMaterial({ color: 0xffffff });
 export const ground = new Mesh(geometry, material);
 
 ground.position.set(0, 0, 0);
-ground.rotation.set(0, degToRad(90), 0);
+ground.rotation.set(degToRad(-90), 0, 0);
