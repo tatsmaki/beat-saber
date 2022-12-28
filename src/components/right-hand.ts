22ee0f29fbdb15badc4ba1226fa8462e944b4a8c
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-12-28 23:36:25 +0100

    fix: right controller size

--- src/components/right-hand.ts
+++ src/components/right-hand.ts
@@ -1,10 +1,11 @@
 import { CylinderGeometry, Group, Mesh, MeshLambertMaterial } from "three";
 import { degToRad } from "three/src/math/MathUtils";
 
-const geometry = new CylinderGeometry(0.01, 0.01, 0.1);
+const geometry = new CylinderGeometry(0.02, 0.02, 0.2);
 const material = new MeshLambertMaterial({ color: "red" });
 
 const mesh = new Mesh(geometry, material);
+// mesh.position.x = -0.1;
 mesh.rotateX(degToRad(-45));
 
 export const rightHand = new Group().add(mesh);
