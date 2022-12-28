222a5aea11ff6ed8b521aea3352887eaf0f10388
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-12-28 23:34:57 +0100

    fix: right hand

--- src/components/right-hand.ts
+++ src/components/right-hand.ts
@@ -1,10 +1,10 @@
 import { CylinderGeometry, Group, Mesh, MeshLambertMaterial } from "three";
 import { degToRad } from "three/src/math/MathUtils";
 
-const geometry = new CylinderGeometry(0.1, 0.1, 0.5);
+const geometry = new CylinderGeometry(0.01, 0.01, 0.1);
 const material = new MeshLambertMaterial({ color: "red" });
 
 const mesh = new Mesh(geometry, material);
-mesh.rotateX(degToRad(45));
+mesh.rotateX(degToRad(-45));
 
 export const rightHand = new Group().add(mesh);
