7100ce0390fb1103c6f82f2bc3e6b8a0c6bd7b96
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-12-29 16:23:56 +0100

    fix: shadow bias & size

--- src/components/sun.ts
+++ src/components/sun.ts
@@ -1,22 +1,26 @@
 import {
   CircleGeometry,
+  DirectionalLight,
   Group,
   Mesh,
-  MeshLambertMaterial,
-  PointLight,
+  MeshBasicMaterial,
 } from "three";
 import { degToRad } from "three/src/math/MathUtils";
 
 const geometry = new CircleGeometry(1, 32);
-const material = new MeshLambertMaterial({ color: 0xfffbdb });
+const material = new MeshBasicMaterial({ color: 0xfffbdb });
 
 const mesh = new Mesh(geometry, material);
 mesh.position.set(-50, 0, 0);
 mesh.rotateY(degToRad(90));
 
-const light = new PointLight(0xfffbdb);
+const light = new DirectionalLight(0xfffbdb);
 light.position.set(-49, 0, 0);
 light.castShadow = true;
+light.lookAt(0, 0, 0);
+light.shadow.bias = -0.0001;
+light.shadow.mapSize.width = 2048;
+light.shadow.mapSize.height = 2048;
 
 export const sun = new Group();
 
