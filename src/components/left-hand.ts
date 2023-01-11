51ebaf2126334bcab72c9b3b935bf7c419e912c0
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-11 12:36:08 +0100

    test: blue light

--- src/components/left-hand.ts
+++ src/components/left-hand.ts
@@ -4,8 +4,10 @@ import {
   Group,
   Mesh,
   MeshLambertMaterial,
+  RectAreaLight,
 } from "three";
 import { degToRad } from "three/src/math/MathUtils";
+import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper";
 
 const grip = new Mesh(
   new CylinderGeometry(0.02, 0.02, 0.2),
@@ -13,13 +15,21 @@ const grip = new Mesh(
 );
 grip.rotateX(degToRad(90));
 
-const blade = new Mesh(
-  new CylinderGeometry(0.02, 0.02, 1),
-  new MeshLambertMaterial({ color: "blue", emissive: new Color("blue") })
-);
-blade.translateZ(-0.5);
-blade.rotateX(degToRad(90));
+// const blade = new Mesh(
+//   new CylinderGeometry(0.02, 0.02, 1),
+//   new MeshLambertMaterial({ color: "blue", emissive: new Color("blue") })
+// );
+// blade.translateZ(-0.5);
+// blade.rotateX(degToRad(90));
+
+const light = new RectAreaLight("blue", 10, 0.02, 1);
+light.translateZ(-0.5);
+light.rotateX(degToRad(90));
+light.castShadow = true;
+const helper = new RectAreaLightHelper(light);
+light.add(helper);
 
 export const leftHand = new Group();
 leftHand.add(grip);
-leftHand.add(blade);
+// leftHand.add(blade);
+leftHand.add(light);
