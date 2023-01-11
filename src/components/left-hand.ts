7e30cb9f690da5635c6af8348af604886606f1ee
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-11 11:25:35 +0100

    test: rect area light

--- src/components/left-hand.ts
+++ src/components/left-hand.ts
@@ -5,7 +5,6 @@ const geometry = new CylinderGeometry(0.02, 0.02, 0.2);
 const material = new MeshLambertMaterial({ color: "blue" });
 
 const mesh = new Mesh(geometry, material);
-// mesh.position.set(0, 0, 0.1);
 mesh.rotateX(degToRad(90));
 
 export const leftHand = new Group();
--- src/components/right-hand.ts
+++ src/components/right-hand.ts
@@ -11,15 +11,14 @@ const geometry = new CylinderGeometry(0.02, 0.02, 0.2);
 const material = new MeshLambertMaterial({ color: "red" });
 
 const mesh = new Mesh(geometry, material);
-// mesh.position.set(0, 0, 0.1);
 mesh.rotateX(degToRad(90));
-
-const light = new RectAreaLight(0xffffff, 1, 0.2, 1);
-light.position.set(0, 0, 0.1);
-light.rotateX(degToRad(90));
-light.lookAt(0, 0, 0);
 
 export const rightHand = new Group();
 
-rightHand.add(light);
 rightHand.add(mesh);
+
+const light = new RectAreaLight(0xffffff, 1, 0.2, 1);
+// light.position.set(0, 0, 0.1);
+light.rotateX(degToRad(90));
+
+rightHand.add(light);
