e55daec8dc10aec3418d3dcb4a76a4e987392360
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-11 12:33:09 +0100

    fix: left controller grip

--- src/frames/left-hand.frame.ts
+++ src/frames/left-hand.frame.ts
@@ -2,7 +2,7 @@ import { renderer } from "../renderer";
 import { leftHand } from "../components/left-hand";
 
 export const leftHandFrame = () => {
-  const leftController = renderer.xr.getController(0);
+  const leftController = renderer.xr.getControllerGrip(0);
   leftHand.position.copy(leftController.position);
   leftHand.rotation.copy(leftController.rotation);
 };
