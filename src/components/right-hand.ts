f5a6a58b10f3c24091889725c257f2aab0140b3e
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-11 11:30:11 +0100

    test: rect light

--- src/components/right-hand.ts
+++ src/components/right-hand.ts
@@ -6,6 +6,7 @@ import {
   RectAreaLight,
 } from "three";
 import { degToRad } from "three/src/math/MathUtils";
+import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper";
 
 const geometry = new CylinderGeometry(0.02, 0.02, 0.2);
 const material = new MeshLambertMaterial({ color: "red" });
@@ -20,5 +21,9 @@ rightHand.add(mesh);
 const light = new RectAreaLight(0xffffff, 1, 0.2, 1);
 // light.position.set(0, 0, 0.1);
 light.rotateX(degToRad(90));
+light.lookAt(0, 0, 0);
+
+const helper = new RectAreaLightHelper(light);
+light.add(helper);
 
 rightHand.add(light);
