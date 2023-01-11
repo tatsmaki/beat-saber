4bc58ed2531833da38744a94e530c2da84f93abe
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-11 12:31:18 +0100

    test: emissive material

--- src/components/left-hand.ts
+++ src/components/left-hand.ts
@@ -1,12 +1,25 @@
-import { CylinderGeometry, Group, Mesh, MeshLambertMaterial } from "three";
+import {
+  Color,
+  CylinderGeometry,
+  Group,
+  Mesh,
+  MeshLambertMaterial,
+} from "three";
 import { degToRad } from "three/src/math/MathUtils";
 
-const geometry = new CylinderGeometry(0.02, 0.02, 0.2);
-const material = new MeshLambertMaterial({ color: "blue" });
+const grip = new Mesh(
+  new CylinderGeometry(0.02, 0.02, 0.2),
+  new MeshLambertMaterial({ color: "blue" })
+);
+grip.rotateX(degToRad(90));
 
-const mesh = new Mesh(geometry, material);
-mesh.rotateX(degToRad(90));
+const blade = new Mesh(
+  new CylinderGeometry(0.02, 0.02, 1),
+  new MeshLambertMaterial({ color: "blue", emissive: new Color("blue") })
+);
+blade.translateZ(-0.5);
+blade.rotateX(degToRad(90));
 
 export const leftHand = new Group();
-
-leftHand.add(mesh);
+leftHand.add(grip);
+leftHand.add(blade);
--- src/components/right-hand.ts
+++ src/components/right-hand.ts
@@ -8,22 +8,19 @@ import {
 import { degToRad } from "three/src/math/MathUtils";
 import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper";
 
-const geometry = new CylinderGeometry(0.02, 0.02, 0.2);
-const material = new MeshLambertMaterial({ color: "red" });
-
-const mesh = new Mesh(geometry, material);
+const mesh = new Mesh(
+  new CylinderGeometry(0.02, 0.02, 0.2),
+  new MeshLambertMaterial({ color: "red" })
+);
 mesh.rotateX(degToRad(90));
-
-export const rightHand = new Group();
-
-rightHand.add(mesh);
 
 const light = new RectAreaLight("red", 10, 0.02, 1);
 light.translateZ(-0.5);
 light.rotateX(degToRad(90));
 light.castShadow = true;
-
 const helper = new RectAreaLightHelper(light);
 light.add(helper);
 
+export const rightHand = new Group();
+rightHand.add(mesh);
 rightHand.add(light);
