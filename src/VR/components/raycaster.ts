72b8630d8d29dc27c58ba89a53f38043aa48f42e
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-18 00:31:17 +0100

    fix: raycaster direction

--- src/VR/components/raycaster.ts
+++ src/VR/components/raycaster.ts
@@ -1,6 +1,3 @@
-import { Raycaster, Vector3 } from "three";
+import { Raycaster } from "three";
 
-const origin = new Vector3(0, 0, 0);
-const direction = new Vector3(0, 0, 0);
-
-export const raycaster = new Raycaster(origin, direction);
+export const raycaster = new Raycaster();
