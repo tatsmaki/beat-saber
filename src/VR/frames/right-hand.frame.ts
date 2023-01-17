342e237d9d17cfdd6a2a112522e9dd0328bc6583
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-17 23:22:45 +0100

    fix: raycaster direction

--- src/VR/frames/right-hand.frame.ts
+++ src/VR/frames/right-hand.frame.ts
@@ -1,3 +1,4 @@
+import { Vector3 } from "three";
 import { renderer } from "../../renderer";
 import { boxes } from "../components/boxes";
 import { raycaster } from "../components/raycaster";
@@ -9,7 +10,7 @@ export const rightHandFrame = () => {
   rightHand.rotation.copy(rightController.rotation);
   raycaster.set(
     rightController.position,
-    rightController.getWorldDirection(rightController.position)
+    new Vector3().applyQuaternion(rightController.quaternion).normalize()
   );
   const [intersection] = raycaster.intersectObject(boxes, true);
   if (intersection && intersection.distance < 2) {
