1a5c60953d8859bf63b41e544aae27c5bc09472d
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-18 01:19:20 +0100

    fix: angle between 2 vectors

--- src/VR/frames/right-hand.frame.ts
+++ src/VR/frames/right-hand.frame.ts
@@ -1,4 +1,4 @@
-import { Matrix4, Vector3 } from "three";
+import { Matrix4, Vector2, Vector3 } from "three";
 import { XrController } from "../../controllers/xr.controller";
 import { renderer } from "../../renderer";
 import { boxes } from "../components/boxes";
@@ -17,11 +17,13 @@ export const rightHandFrame = (xrController: XrController) => {
   boxes.children.forEach((box) => {
     const isNear =
       box.getWorldPosition(new Vector3()).distanceTo(position) < 0.5;
-    const direction = new Vector3(angularVelocity.x, angularVelocity.y);
-    const angle = direction.angleTo(
-      new Vector3(box.userData.d.x || 0, -box.userData.d.y || 0)
+    const direction = new Vector2(angularVelocity.x, angularVelocity.y);
+    const boxDirection = new Vector2(
+      box.userData.d.x || 0,
+      box.userData.d.y || 0
     );
-    if (isNear && Math.abs(angle) < 0.3) {
+    const angle = Math.abs(direction.angle() - boxDirection.angle());
+    if (isNear && angle < 0.45) {
       box.removeFromParent();
       xrController.makePulse();
     }
