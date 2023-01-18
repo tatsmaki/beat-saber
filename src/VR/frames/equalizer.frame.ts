9419683b01fc09b705bb609fe74d96fb6e718989
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-18 20:32:01 +0100

    feat: smooth equalizer

--- src/VR/frames/equalizer.frame.ts
+++ src/VR/frames/equalizer.frame.ts
@@ -1,10 +1,10 @@
-import { Vector3 } from "three";
-import throttle from "lodash.throttle";
+import { Quaternion, Vector3 } from "three";
 import { equalizer, equalizerChildren } from "../components/equalizer";
 
 const rv3 = new Vector3(0, 1.5, 0);
+const za = new Vector3(0, 0, 1);
 
-export const equalizerFrame = throttle((uint8: Uint8Array, hv3: Vector3) => {
+export const equalizerFrame = (uint8: Uint8Array, hv3: Vector3) => {
   let avg = 0;
   for (let i = 0; i < uint8.length; i += 1) {
     avg += uint8[i];
@@ -13,8 +13,13 @@ export const equalizerFrame = throttle((uint8: Uint8Array, hv3: Vector3) => {
   for (let i = 0; i < uint8.length; i += 1) {
     const value = (uint8[i] - avg) / 16;
     const { position } = equalizerChildren[i];
-    position.lerp(new Vector3(position.x, value, position.z), 0.5);
+    position.lerp(new Vector3(position.x, value, position.z), 0.2);
   }
   const angle = Math.atan2(hv3.y - rv3.y, hv3.x - rv3.x);
-  equalizer.rotation.z = angle;
-}, 40);
+  equalizer.quaternion.slerp(
+    new Quaternion()
+      .setFromEuler(equalizer.rotation)
+      .setFromAxisAngle(za, angle),
+    0.05
+  );
+};
